cf30f373b6308acc0eb1df9744a5b4f7879291f1
JimHaff <james.haff@generalassemb.ly>
2025-01-23 14:12:44 -0600

    added headers

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,15 +1,45 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
+const UserModel = require("../models/user");
+const User = require("../models/user");
 
-router.get('/', function(req, res){
-	console.log(req.session, ' req.session in index of applications')
-	res.render('applications/index.ejs')
-})
+router.get("/", async function (req, res) {
+  console.log(req.session, " req.session in index of applications");
+  try {
+    const currentUser = await User.findById(req.session.user._id);
 
-router.get('/new', function(req, res){
-	res.render('applications/new.ejs')
-})
+    res.render("applications/index.ejs", {
+      applications: currentUser.applications,
+    });
+	
+  } catch (err) {
+    console.log(err);
+    res.send("Error Rendering all applications check terminal");
+  }
+});
+
+router.get("/new", function (req, res) {
+  res.render("applications/new.ejs");
+});
+
+router.post("/", async function (req, res) {
+  try {
+    // look up the user
+    const currentUser = await User.findById(req.session.user._id);
+    // then add the contents of the form to the users applications array
+    currentUser.applications.push(req.body);
+    // anytime we mutate a document we must tell the database
+    // by calling .save
+    await currentUser.save();
+    console.log(currentUser, " <- currentUser");
+    // redirect back to the index route
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (err) {
+    console.log(err);
+    res.send("Error check the terminal to debug");
+  }
+});
 
 // we need to mount the router in our server.js in order to use it!
-module.exports = router
+module.exports = router;
